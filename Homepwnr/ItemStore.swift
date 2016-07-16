9df21d1abdadd2b881e20278b22182bdd93d9fcf
David Lawrence <dlawrence@focusedconcepts.com>
2016-07-16 11:01:25 -0400

    Homepwnr v1.1 b0: Build model classes for table view, starting in iOS Programming book "Creating the Item Class" section, through section "Creating and Retrieving UITableView cells", so when run, the table view should be populated with a list of random items. It is not. An error occurs in the app delegate, stating that the view obtained from the window object cannot be cast to type ItemsViewController.

---
+++ Homepwnr/ItemStore.swift
@@ -0,0 +1,27 @@
+//
+//  ItemStore.swift
+//  Homepwnr
+//
+//  Created by David Lawrence on 7/12/16.
+//  Copyright © 2016 focusedConcepts. All rights reserved.
+//
+
+import UIKit
+
+class ItemStore {
+    var allItems = [Item]()
+    
+    init() {
+        for _ in 0..<5 {
+            createItem()
+        }
+    }
+    
+    func createItem() -> Item {
+        let newItem = Item(random: true)
+        
+        allItems.append(newItem)
+        
+        return newItem
+    }
+}
